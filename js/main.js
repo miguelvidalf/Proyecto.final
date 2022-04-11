2b1f107cbe89571b68d6f5d7da1c7f9fd3bc922b
KrhisnaO <101838649+KrhisnaO@users.noreply.github.com>
2022-04-10 23:45:58 -0400

    Update main.js (#9)

--- js/main.js
+++ js/main.js
@@ -132,5 +132,22 @@
         ],
     });
     
+    //*** Quantity Start ***//
+$('.qty button').on('click', function () {
+    var $button = $(this);
+    var oldValue = $button.parent().find('input').val();
+    if ($button.hasClass('btn-plus')) {
+        var newVal = parseFloat(oldValue) + 1;
+    } else {
+        if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+        } else {
+            newVal = 0;
+        }
+    }
+    $button.parent().find('input').val(newVal);
+});
+//*** Quantify End ***//
+    
 })(jQuery);
 
